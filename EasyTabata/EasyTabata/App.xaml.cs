b1e719528a442e6314bfc22840c517d9a95d2435
winux <jpalbertini@live.fr>
2020-06-04 23:20:33 +0200

    add appcenter specifics, use proper certificates

--- EasyTabata/EasyTabata/App.xaml.cs
+++ EasyTabata/EasyTabata/App.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using Xamarin.Forms;
 using EasyTabata.Views;
 using EasyTabata.Services;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 
 namespace EasyTabata
 {
@@ -18,6 +21,7 @@ namespace EasyTabata
 
         protected override void OnStart()
         {
+            AppCenter.Start("android=9d12b6bd-1aad-49f6-b720-254f959b3c38;" + "uwp=61e0de10-0c19-48e6-8959-43458c05bbed;" + "ios=ecbf269e-c12d-4ce2-ad4d-98d8389566fa", typeof(Analytics), typeof(Crashes));
         }
 
         protected override void OnSleep()
